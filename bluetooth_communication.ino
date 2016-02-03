c5f265e92dd02b73eaef2d4890cd0c2266bf1ac1
Arafat245 <arafatrahman245@outlook.com>
2016-02-03 16:16:51 +0600

    First C# app

---
+++ bluetooth_communication.ino
@@ -0,0 +1,14 @@
+#include <SoftwareSerial.h>
+SoftwareSerial myconnection(10,11);
+
+void setup()
+{
+  myconnection.begin(9600);
+}
+
+void loop()
+{
+  myconnection.println("My name is Arafat");
+  delay(600);
+}
+
